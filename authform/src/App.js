f20c67395eb5e1784fc3097dc1e25d3bb68f3443
Ken Krueger <kenkrueger65@gmail.com>
2024-10-31 13:30:04 -0400

    auth form

---
+++ authform/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Contact from './contact.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
